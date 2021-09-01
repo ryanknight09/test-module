2542abf024a4f2f2f7b657123e4fbeb7a0defad3
Ryan Knight <ryan.knight@evolvemep.com>
2021-09-01 12:36:15 -0600

    init

---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <div>
+      <header>
+        <p>Test Module</p>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
---
+++ webpack.config.js
@@ -0,0 +1,123 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+// const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+const deps = require('./package.json').dependencies;
+
+module.exports = {
+  entry: './src/index',
+  cache: true,
+
+  mode: 'development',
+  devtool: 'source-map',
+
+  optimization: {
+    minimize: false,
+    runtimeChunk: 'single'
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/'
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
+    alias: {}
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx?$/,
+        resolve: {
+          fullySpecified: false
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                javascriptEnabled: true
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|ico)$/i,
+        use: ['file-loader?name=[name].[ext]']
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'nav',
+      filename: 'remoteEntry.js',
+      remotes: {},
+      exposes: {
+        './Header': './src/Header'
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          eager: true,
+          requiredVersion: deps.react
+        },
+        'react-dom': {
+          singleton: true,
+          eager: true,
+          requiredVersion: deps['react-dom']
+        }
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico'
+    })
+  ],
+
+  devServer: {
+    port: 3001,
+    historyApiFallback: true
+  }
+};
